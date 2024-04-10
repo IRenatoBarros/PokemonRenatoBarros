8529ea05c59abbb90e7e00d9f4db18b562f28df7
IRenatoBarros <renatofilho9722@gmail.com>
2024-04-10 20:36:05 -0300

    colocando dicas

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const input = document.getElementById("input")
 const teste = document.getElementById('teste')
 const form = document.getElementById("forms")
 const arrayCoracoes = document.getElementsByTagName('td')
-var numeroAleatorio = Math.floor(Math.random() * 150) + 1;
+const dica1 = document.getElementById("dica1")
+const dica2 = document.getElementById("dica2")
+var numeroAleatorio = Math.floor(Math.random() * 151) + 1;
 let controle = 0;
 let blurr = 3;
 
@@ -12,19 +14,27 @@ fetch('https://pokeapi.co/api/v2/pokemon/'+numeroAleatorio)
   .then(data => {
     console.log(data); // Dados da API
     imagem.setAttribute("src",data.sprites.front_default)
+    dica1.addEventListener('click',()=>{
+      const namee = data.name 
+      teste.textContent = "3 primeiras letras: "+namee.slice(0,3)
+    })
+    dica2.addEventListener('click',()=>{
+      teste.textContent = "Tipo: "+data.types[0].type.name
+    })
+    
     form.addEventListener("submit",(event)=>{
         if(input.value==data.name){
             imagem.style.filter = "blur(0px)"
+            teste.textContent = "Correto (;"
             setTimeout(function(){
               window.location.reload()
             },3000)
             
             
         }else{
-            const namee = data.name 
-            teste.textContent = "Nome começa com -> "+namee.slice(0,controle+1)
             imagem.style.filter = "blur("+blurr+"px)"
             arrayCoracoes[arrayCoracoes.length-1].remove()
+            
         }
         
         
@@ -40,11 +50,14 @@ fetch('https://pokeapi.co/api/v2/pokemon/'+numeroAleatorio)
         imagem.style.filter = "blur("+blurr+")"
         
     })
+
     
   })
   .catch(error => {
     console.error('Ocorreu um erro ao obter os dados:', error);
   });
+
+
 
 
  
