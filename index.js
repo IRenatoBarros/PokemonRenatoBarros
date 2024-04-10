352c5536bb907a2fc66b550777bc8afa91e9aedc
IRenatoBarros <renatofilho9722@gmail.com>
2024-04-10 12:02:46 -0300

    colocando os corações

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const input = document.getElementById("input")
 const teste = document.getElementById('teste')
 const resposta = document.getElementById("resposta")
 const form = document.getElementById("forms")
-var numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-let controle = 0
+const arrayCoracoes = document.getElementsByTagName('td')
+var numeroAleatorio = Math.floor(Math.random() * 250) + 1;
+let controle = 0;
+let blurr = 3;
 
 fetch('https://pokeapi.co/api/v2/pokemon/'+numeroAleatorio)
   .then(response => response.json())
@@ -15,23 +17,30 @@ fetch('https://pokeapi.co/api/v2/pokemon/'+numeroAleatorio)
         if(input.value==data.name){
             imagem.style.filter = "blur(0px)"
             resposta.textContent = 'Correta (;'
-        
-            window.location.reload();
+            setTimeout(function(){
+              window.location.reload()
+            },2000)
+            
+            
         }else{
             resposta.textContent = 'Incorreta ):'   
             teste.textContent = data.name
-            imagem.style.filter = "blur(2px)"
-
+            imagem.style.filter = "blur("+blurr+"px)"
+            arrayCoracoes[arrayCoracoes.length-1].remove()
         }
-        event.preventDefault();
-
+        
+        
         controle++
-
-        if(controle<4){
+        console.log(controle)
+        console.log(blurr)
+        if(controle<3){
             event.preventDefault();
+            blurr--
         }else{
-            window.location.reload();
+          blurr--
         }
+        imagem.style.filter = "blur("+blurr+")"
+        
     })
     
   })
